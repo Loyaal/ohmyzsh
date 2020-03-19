72e30d894e7fc64564357a894258f64bc8d0616a
safety <alexandre.pannetier53@gmail.com>
2020-03-19 01:41:05 +0100

    Add custom zsh files

---
+++ custom/alias.zsh
@@ -0,0 +1,11 @@
+# apt alias
+alias apti="sudo apt-get install"
+alias aptif="sudo apt-get install -y"
+alias aptr="sudo apt-get remove"
+alias apts="apt search"
+
+# zsh alias
+alias zshsrc="source $HOME/.zshrc"
+alias zsheditsrc="nano $HOME/.zshrc"
+alias zshalias="nano $HOME/.oh-my-zsh/custom/alias.zsh"
+alias zshenv="nano $HOME/.oh-my-zsh/custom/env.zsh"
