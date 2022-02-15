d90aaf863811f46548af2c262ba75e7bbaf25940
Loyaal <loyal@exemple.com>
2022-02-15 23:25:23 +0100

    Remove autojump and pure prompt

--- custom/env.zsh
+++ custom/env.zsh
@@ -1,16 +1,7 @@
 export PATH=$PATH:$HOME/bin
 
-
-# Pure prompt config
-fpath+=$HOME/.oh-my-zsh/pure
-autoload -U promptinit; promptinit
-prompt pure
-
 # ANSIWEATHER conf
 export ANSIWEATHERRC="$ZSH_CUSTOM/ansiweatherrc"
-
-# autojump plugin zsh Conf
-source /usr/share/autojump/autojump.sh
 
 # enhancd plugin zsh conf
 export ENHANCD_FILTER=fzy
