7f3e1f1864be37433399190bed6a2beeaf749f07
Loyaal <loyaal@email.com>
2023-04-29 22:14:14 +0200

    Add command lines to support new tools

--- custom/env.zsh
+++ custom/env.zsh
@@ -1,3 +1,12 @@
 export PATH=$PATH:$HOME/bin
 
 
+
+# Pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+# Pyenv-virtualenv
+eval "$(pyenv virtualenv-init -)"
+# Nord dircolors
+test -r ~/.dircolors && eval $(dircolors ~/.dircolors)
